54ae22aa05bcb429e2d3a21c97bf0b7f204f4245
Hoang Viet <nguyenhoangviet170500@gmail.com>
2021-10-13 09:40:19 +0700

    update

--- connector.py
+++
@@ -1,9 +0,0 @@
-import pymysql.cursors
-
-con = pymysql.connect(
-            host = '103.56.156.1999',
-            user = 'root',
-            password = '123456',
-            database = 'updownloadfile',
-            cursorclass = pymysql.cursors.DictCursor
-            )
